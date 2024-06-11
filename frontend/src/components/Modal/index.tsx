745df44afdb217f85fa45820afced4a308a03df0
kalczugag <kalczugag@gmail.com>
2024-06-11 15:21:20 +0200

    modal for profile picture

--- frontend/src/components/Modal/index.tsx
+++ frontend/src/components/Modal/index.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from "react";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 
 interface ModalProps {
@@ -16,7 +15,6 @@ const style = {
     transform: "translate(-50%, -50%)",
     width: 400,
     bgcolor: "background.paper",
-    border: "2px solid #000",
     boxShadow: 24,
     p: 4,
 };
--- frontend/src/pages/Profile/card/index.tsx
+++ frontend/src/pages/Profile/card/index.tsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useGetEmployeeQuery } from "../../../store";
-import { EmailOutlined, Edit } from "@mui/icons-material";
+import { EmailOutlined } from "@mui/icons-material";
 import { Tooltip, IconButton } from "@mui/material";
 import DefaultPage from "../../../layout/DefaultPage";
 import Modal from "../../../components/Modal";
 
 const ProfileCard = () => {
     const { id } = useParams();
-    const [mouseOver, setMouseOver] = useState<boolean>(false);
-    const [open, setOpen] = useState<boolean>(false);
+    const [openEmail, setOpenEmail] = useState<boolean>(false);
+    const [openImg, setOpenImg] = useState<boolean>(false);
 
     const { data } = useGetEmployeeQuery({ id: id! });
 
@@ -17,23 +17,15 @@ const ProfileCard = () => {
         <>
             <DefaultPage breadCrumbs={false} bg>
                 <div className="flex flex-col items-center space-y-24">
-                    <div
-                        onMouseOver={() => setMouseOver(true)}
-                        onMouseLeave={() => setMouseOver(false)}
-                        className="relative bg-primary p-16 w-full hover:bg-primary/[.90]"
-                    >
-                        <img
-                            src={data?.img}
-                            alt="profile pic"
-                            className="w-[200px] h-[200px] mx-auto -mb-32 rounded-full border-2 border-white object-cover"
-                        />
-                        {mouseOver && (
-                            <div className="absolute right-2 bottom-2">
-                                <IconButton>
-                                    <Edit className="text-gray-300" />
-                                </IconButton>
-                            </div>
-                        )}
+                    <div className="relative bg-primary p-16 w-full">
+                        <div className="w-[200px] h-[200px] mx-auto -mb-32 rounded-full bg-white">
+                            <img
+                                src={data?.img}
+                                alt="profile pic"
+                                onClick={() => setOpenImg(true)}
+                                className="w-[200px] h-[200px] object-cover rounded-full border-white border-2 hover:opacity-90 hover:cursor-pointer"
+                            />
+                        </div>
                     </div>
                     <div className="flex flex-col items-center space-y-2">
                         <h3 className="text-2xl">{`${data?.firstName} ${data?.lastName}`}</h3>
@@ -41,7 +33,7 @@ const ProfileCard = () => {
                             <Tooltip title="Send Email">
                                 <IconButton
                                     // href={`mailto:${data?.email}?subject=Email%20Subject&body=Email%20Body`}
-                                    onClick={() => setOpen(true)}
+                                    onClick={() => setOpenEmail(true)}
                                 >
                                     <EmailOutlined />
                                 </IconButton>
@@ -50,7 +42,10 @@ const ProfileCard = () => {
                     </div>
                 </div>
             </DefaultPage>
-            <Modal open={open} handleClose={() => setOpen(false)}>
+            <Modal open={openImg} handleClose={() => setOpenImg(false)}>
+                <img src={data?.img} alt="profile pic" />
+            </Modal>
+            <Modal open={openEmail} handleClose={() => setOpenEmail(false)}>
                 <h6 className="text-xl">Email to {data?.email}</h6>
             </Modal>
         </>
